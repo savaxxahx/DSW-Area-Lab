6bf748abeec31d5c8513d48bc9274b976cc06fda
Sky Adams <skyadams@sbunified.org>
2017-09-25 16:47:47 -0700

    reordered functions

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,7 +1,7 @@
 import math
 import unittest
 
-def triArea(base,height):
+def circleArea(radius):
 	return 1;
 
 def rectArea(base, height):
@@ -9,16 +9,17 @@ def rectArea(base, height):
 
 def trapArea(base1, base2, height):
 	return 1;
-	
-def circleArea(radius):
+
+def triArea(base,height):
 	return 1;
+    
 	
 class MyTest(unittest.TestCase):
-    def testTriArea(self):
+    def testCircleArea(self):
     
     def testRectArea(self):
     
     def testTrapArea(self):
-        
-    def testCircleArea(self):
+    
+    def testTriArea(self):
     
