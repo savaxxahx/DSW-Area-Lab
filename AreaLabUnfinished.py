172e868de37c6878236bc43b4b35557b077faa8e
Sky Adams <skyadams@sbunified.org>
2017-09-25 16:22:56 -0700

    fixed tabs and added return statements

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -2,25 +2,23 @@ import math
 import unittest
 
 def triArea(base,height):
-
+	return 1;
 
 def rectArea(base, height):
-
+	return 1;
 
 def trapArea(base1, base2, height):
-
+	return 1;
 	
 def circleArea(radius):
-
+	return 1;
 	
 class MyTest(unittest.TestCase):
     def testTriArea(self):
-	
-	
+    
     def testRectArea(self):
-	
-
+    
     def testTrapArea(self):
-	
-
+        
     def testCircleArea(self):
+    
