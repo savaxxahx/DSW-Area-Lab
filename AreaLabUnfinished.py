7efa2690ff9e4d72f37e23efe2982f39c23cf840
sky-adams <skyadams@sbunified.org>
2017-09-25 15:53:44 -0700

    Create AreaLabUnfinished.py

---
+++ AreaLabUnfinished.py
@@ -0,0 +1,26 @@
+import math
+import unittest
+
+def triArea(base,height):
+
+
+def rectArea(base, height):
+
+
+def trapArea(base1, base2, height):
+
+	
+def circleArea(radius):
+
+	
+class MyTest(unittest.TestCase):
+    def testTriArea(self):
+	
+	
+    def testRectArea(self):
+	
+
+    def testTrapArea(self):
+	
+
+    def testCircleArea(self):
